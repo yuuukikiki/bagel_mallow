86c115fd30bb7a440cd18d22af6a44e693eeb537
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-08 06:23:37 +0900

    カート関連のアソシエーション設定追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  has_many :order_items
+  has_many :cart_items
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   belongs_to :user
+  belongs_to :address
 end
