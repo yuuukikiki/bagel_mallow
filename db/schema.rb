823e0d70e42fa1faf9d6be6fc94b928342a2a409
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-09 20:02:40 +0900

    contactsテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_07_073646) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_09_054453) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -66,6 +66,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_07_073646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "contacts", charset: "utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.text "message", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", charset: "utf8", force: :cascade do |t|
