fccf967b214c3baaf04979fcfdb19c53eac9ff22
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-10 04:40:54 +0900

    お問い合わせのコントローラーとルーティングの設定

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,21 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:notice] = 'お問い合わせが送信されました'
+      redirect_to new_contact_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
   end
 end
