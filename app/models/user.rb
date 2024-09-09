d48fba2d95983a1e2c8b04701031c3dfd27d5b1f
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-10 08:24:08 +0900

    userモデルのバリデーションの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,9 @@ class User < ApplicationRecord
   has_many :addresses
 
   validates :name, presence: { message: I18n.t('errors.messages.blank') }
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: I18n.t('errors.messages.phone_number') }
+  validates :phone_number,
+            format: { with: /\A(\d{10,11}|\d{2,4}-\d{2,4}-\d{4})\z/, message: I18n.t('errors.messages.phone_number') }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/, message: I18n.t('errors.messages.password_invalid') }
-  validates :email, presence: { message: I18n.t('errors.messages.blank') },
-                    uniqueness: { message: I18n.t('errors.messages.taken') }
+  validates :email, presence: true,
+                    uniqueness: true
 end
