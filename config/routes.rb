5505dfd50d02ada1e5a7ea1c2cca9514c023e27e
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-10 08:26:44 +0900

    カート関連のルーティング、コントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
   resources :cart_items, only: [:create, :update, :destroy]
 
   # 注文関連のルーティング
-  resources :orders, only: [:index, :new, :create]
+  resources :orders, only: [:index, :new, :create] do
+    collection do
+      get 'complete'
+    end
+  end
 
   # ユーザー認証関連のルーティング
   devise_for :users
