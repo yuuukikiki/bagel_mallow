2b52b78883a83a297ea5ab4c2fef0a2f2fb09423
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-13 08:30:26 +0900

    cart_itemモデルの編集

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,4 +4,9 @@ class CartItem < ApplicationRecord
 
   validates :quantity, presence: { message: I18n.t('errors.messages.blank') },
                        numericality: { only_integer: true, greater_than: 0, message: I18n.t('errors.messages.greater_than_or_equal_to') }
+
+  # アイテムの合計金額を計算するメソッド
+  def total_price
+    item.price * quantity
+  end
 end
