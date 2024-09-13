ab856c8c1f7a4cda705b23512733673cf778a28e
[tatsuyakamada] <[t.kamada1151@gmail.com]>
2024-09-13 11:38:40 +0900

    カートの修正対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_10_063151) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_13_005201) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_10_063151) do
   create_table "cart_items", charset: "utf8", force: :cascade do |t|
     t.bigint "cart_id", null: false
     t.bigint "item_id", null: false
-    t.integer "quantity", null: false
+    t.integer "quantity", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_cart_items_on_cart_id"
