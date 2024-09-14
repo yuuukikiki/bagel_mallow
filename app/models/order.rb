56029114c918c0845cbaa342f0f6a484134e79a1
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-14 22:25:02 +0900

    モデルのアソシエーションの変更

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,12 @@
 class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   belongs_to :user
-  belongs_to :address
+  has_one :address, dependent: :destroy
 
-  validates :address_id, presence: { message: I18n.t('errors.messages.blank') }
+  accepts_nested_attributes_for :order_items
+  accepts_nested_attributes_for :address
+
+  validates :address, presence: { message: I18n.t('errors.messages.blank') }
   validates :total_amount, presence: { message: I18n.t('errors.messages.blank') },
                            numericality: { greater_than: 0, message: I18n.t('errors.messages.greater_than') }
 end
