4e39a5bbc62175aec97d69b855376363f4e4d3b3
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-16 08:23:53 +0900

    注文ページのコントローラー変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,31 +1,17 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @items = Item.all
-  end
-
   def new
     @order = Order.new
-    @order.build_address
-
-    # デバッグ: アドレスがちゃんと作られているか確認
-    puts @order.address.inspect
-
-    return unless current_user.orders.any?
-
-    last_order = current_user.orders.order(created_at: :desc).first
-    return unless last_order.address
-
-    @order.build_address(last_order.address.attributes.slice(
-                           'postal_code', 'prefecture_id', 'city', 'street', 'building_name', 'phone_number'
-                         ))
+    @order.build_address # Addressオブジェクトの初期化
   end
 
   def create
-    @order = current_user.orders.build(order_params)
+    @order = Order.new(order_params)
+    @order.user = current_user
+
     if @order.save
-      redirect_to complete_orders_path, notice: '注文が完了しました'
+      redirect_to complete_orders_path, notice: '注文が正常に作成されました。'
     else
       render :new
     end
@@ -39,7 +25,7 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(
       address_attributes: [:postal_code, :prefecture_id, :city, :street, :building_name, :phone_number],
-      order_items_attributes: [:item_id, :quantity] # order_items_attributes を追加
+      order_items_attributes: [:item_id, :quantity] # 商品情報のネストされた属性をパーミット
     )
   end
 end
