3505606dabc1e50eb3666bfc72588264acd26918
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-16 14:32:05 +0900

    マイグレーションファイルの設定変更、カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2024_09_13_005201) do
-
-ActiveRecord::Schema[7.0].define(version: 2024_09_13_035507) do
-
+ActiveRecord::Schema[7.0].define(version: 2024_09_16_044603) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,7 +48,23 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_13_035507) do
     t.integer "prefecture_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "order_id", null: false
+    t.string "phone_number"
+    t.index ["order_id"], name: "index_addresses_on_order_id"
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "admins", charset: "utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "cart_items", charset: "utf8", force: :cascade do |t|
@@ -126,6 +138,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_13_035507) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "orders"
   add_foreign_key "addresses", "users"
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "items"
