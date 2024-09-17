baaac91b1be903d13ee1e29842a1da3e99593fed
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-17 10:34:34 +0900

    コントローラーの設定，変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @cart = current_user.cart
+    @cart_items = @cart.cart_items.includes(:item) if @cart
     @items = Item.all
   end
 
@@ -9,8 +11,8 @@ class OrdersController < ApplicationController
     @order = Order.new
     @order.build_address
 
-    # デバッグ: アドレスがちゃんと作られているか確認
-    puts @order.address.inspect
+    # Calculate total amount from cart items
+    @total_amount = current_user.cart.cart_items.sum { |cart_item| cart_item.item.price * cart_item.quantity }
 
     return unless current_user.orders.any?
 
@@ -24,14 +26,20 @@ class OrdersController < ApplicationController
 
   def create
     @order = current_user.orders.build(order_params)
+    @cart = current_user.cart
+    @total_amount = @cart.cart_items.sum { |item| item.quantity * item.item.price }
+
+    @order.total_amount = @total_amount
+
     if @order.save
-      redirect_to complete_orders_path, notice: '注文が完了しました'
+      redirect_to complete_orders_path(@order), notice: '注文が完了しました'
     else
       render :new
     end
   end
 
   def complete
+    @order = Order.find(params[:id])
   end
 
   private
@@ -39,7 +47,8 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(
       address_attributes: [:postal_code, :prefecture_id, :city, :street, :building_name, :phone_number],
-      order_items_attributes: [:item_id, :quantity] # order_items_attributes を追加
+      order_items_attributes: [:item_id, :quantity],
+      total_amount: []
     )
   end
 end
