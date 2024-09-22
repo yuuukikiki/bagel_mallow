ade4b1d4098cb456c703d1c685c1a9324e8aa534
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-22 23:16:51 +0900

    CSPの設定修正

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,28 +2,16 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
-    # デフォルトのリソースロード許可
     policy.default_src :self, :https
-
-    # フォントの読み込みを許可（Google Fonts用）
     policy.font_src    :self, :https, 'https://fonts.googleapis.com', 'https://fonts.gstatic.com'
-
-    # 画像の読み込みを許可
     policy.img_src     :self, :https, :data
-
-    # オブジェクト埋め込みの禁止
     policy.object_src  :none
-
-    # スクリプトの読み込みを許可（PAY.JPのスクリプトも含む）
     policy.script_src  :self, :https, 'https://js.pay.jp'
-
-    # スタイルシートの読み込みを許可し、インラインスタイルを許可
     policy.style_src   :self, :https, 'https://fonts.googleapis.com', "'unsafe-inline'"
-
+    policy.style_src_elem :self, :https, 'https://fonts.googleapis.com'  # これを追加
     # policy.report_uri "/csp-violation-report-endpoint"
   end
 
-    # インラインスクリプト・スタイルでのnonce生成
   config.content_security_policy_nonce_generator = ->(request) { SecureRandom.base64(16) }
   config.content_security_policy_nonce_directives = %w(script-src)
 
