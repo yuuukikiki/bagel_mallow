55c584b3db8ce519c21b590744d74a743292e21e
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-09-23 13:39:17 +0900

    before_actionの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @cart = current_user.cart
